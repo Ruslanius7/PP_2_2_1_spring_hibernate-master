0d196ed869cef49846cd1379ebf9c6330f36a500
Ruslan Perepelitsa <ruslanperepelitsa77@gmail.com>
2024-11-26 13:31:50 +0300

    All bean-dependencies injected with setters

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -13,28 +13,32 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   public List<User> findUserByCar(String model, int series) {
-      Query<User> query = sessionFactory.getCurrentSession().createQuery(
-              "FROM User u WHERE u.car.model = :model AND u.car.series = :series", User.class);
-      query.setParameter("model", model);
-      query.setParameter("series", series);
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public List<User> findUserByCar(String model, int series) {
+        Query<User> query = sessionFactory.getCurrentSession().createQuery(
+                "FROM User u WHERE u.car.model = :model AND u.car.series = :series", User.class);
+        query.setParameter("model", model);
+        query.setParameter("series", series);
+        return query.getResultList();
+    }
 
 }
